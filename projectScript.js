97ec9a78ac6318d302fe658c69e911e7ef8f512f
VolcanoCookies <franek.kg@gmail.com>
2019-12-14 13:49:40 +0100

    Automatic project loading from JSON

--- projectScript.js
+++ projectScript.js
@@ -3,18 +3,19 @@
 var projectContainer;
 var projects;
 
+var template = '<li class="project-list-item"><a class="project-item-link" href="project_link"><img class="project-image" src="project_image" alt="Project Image Unavailable"><hr class="project-description"><h2 class="project-title">project_title</h2><hr class="project-description"><p class="project-description">project_description</p></a></li>';
+
 window.addEventListener('DOMContentLoaded', function() {
 	
-	projectContainer = document.getElementById('project-container');
+	projectContainer = document.getElementById('project-list');
 	
-	projects = JSON.parse(Get('https://api.myjson.com/bins/ouq54'));
+	projects = JSON.parse(Get('https://raw.githubusercontent.com/VolcanoCookies/Portfolio-Website/master/projects.json'));
 	
 	for(i in projects.projects) {
+		
 		var project = projects.projects[i];
-		var title = project.title;
-		var description = project.description;
-		var image = project.image;
-		console.log("Title: " + title + "\nDesciption: " + description + "\nImage: " + image);
+		
+		createProject(project);
 	}
 	
 }, false);
@@ -24,4 +25,27 @@ function Get(yourUrl){
 	Httpreq.open("GET",yourUrl,false);
 	Httpreq.send(null);
 	return Httpreq.responseText;          
+}
+
+function createProject(project) {
+	
+	var title = project.title;
+	var description = project.description;
+	var image = project.image;
+	var link = project.link;
+	
+	var element = document.createElement('project');
+	var html = template.trim();
+	html = html.replace('project_title', title);
+	html = html.replace('project_description', description);
+	html = html.replace('project_image', image);
+	html = html.replace('project_link', link);
+	
+	element.innerHTML = html;
+	
+	console.log("Title: " + title + "\nDesciption: " + description + "\nImage: " + image);
+	console.log(element.innerHTML);
+	
+	projectContainer.innerHTML += element.innerHTML;
+	
 }
