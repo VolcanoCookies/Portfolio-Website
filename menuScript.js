4bbaeabafe1c265532bbcfa5618e38a889a4c63f
VolcanoCookies <franek.kg@gmail.com>
2019-12-23 12:21:05 +0100

    Removed uneccesary navbar elements

--- menuScript.js
+++ menuScript.js
@@ -2,6 +2,6 @@
 
 $(window).on('ready', function() {
 	
-	$("#nav-placeholder").load("https://raw.githubusercontent.com/VolcanoCookies/Portfolio-Website/master/nav.html");
+	$("#nav-placeholder").load("https://raw.githubusercontent.com/VolcanoCookies/Portfolio-Website/static-navbar/nav.html");
 	
 });
