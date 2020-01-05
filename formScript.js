a03d2538c406c35ccca01e2d2116e94ad38d37c5
VolcanoCookies <franek.kg@gmail.com>
2020-01-05 13:54:50 +0100

    update

--- formScript.js
+++ formScript.js
@@ -1,76 +1,57 @@
 // JavaScript Document
 
-
 $(window).on('load', function() {
-	
 	$('#email-field').on('focusin', function() {
-		
 		var field = $(this).context;
-		
-		if(field.value.trim() == 'Email') {
-			field.value = '';
-		}
-		
-		field.style.color = 'black';
-		
-	});
-	
-	$('#email-field').on('focusout', function() {
-		
-		var field = $(this).context;
-		
-		if(field.value.trim() == '') {
-			field.value = 'Email';
-			field.style.color = '#ABABAB';
-		}
-		
-	});
-	
-	$('#subject-field').on('focusin', function() {
-		
-		var field = $(this).context;
-		
-		if(field.value.trim() == 'Subject') {
-			field.value = '';
-		}
-		
-		field.style.color = 'black';
-		
-	});
-	
-	$('#subject-field').on('focusout', function() {
-		
-		var field = $(this).context;
-		
-		if(field.value.trim() == '') {
-			field.value = 'Subject';
-			field.style.color = '#ABABAB';
-		}
-		
-	});
-	
-	$('#message-field').on('focusin', function() {
-		
-		var field = $(this).context;
-		
-		if(field.value.trim() == 'Message') {
-			field.value = '';
-		}
-		
-		field.style.color = 'black';
-		
-	});
-	
-	$('#message-field').on('focusout', function() {
-		
-		var field = $(this).context;
-		
-		if(field.value.trim() == '') {
-			field.value = 'Message';
-			field.style.color = '#ABABAB';
-		}
-		
-	});
-	
-});
 
+		if (field.value.trim() == 'Email') {
+			field.value = '';
+		}
+
+		field.style.color = 'black';
+	});
+
+	$('#email-field').on('focusout', function() {
+		var field = $(this).context;
+
+		if (field.value.trim() == '') {
+			field.value = 'Email';
+		}
+	});
+
+	$('#subject-field').on('focusin', function() {
+		var field = $(this).context;
+
+		if (field.value.trim() == 'Subject') {
+			field.value = '';
+		}
+
+		field.style.color = 'black';
+	});
+
+	$('#subject-field').on('focusout', function() {
+		var field = $(this).context;
+
+		if (field.value.trim() == '') {
+			field.value = 'Subject';
+		}
+	});
+
+	$('#message-field').on('focusin', function() {
+		var field = $(this).context;
+
+		if (field.value.trim() == 'Message') {
+			field.value = '';
+		}
+
+		field.style.color = 'black';
+	});
+
+	$('#message-field').on('focusout', function() {
+		var field = $(this).context;
+
+		if (field.value.trim() == '') {
+			field.value = 'Message';
+		}
+	});
+});
