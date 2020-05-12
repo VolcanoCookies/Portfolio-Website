96516fad29d0daac4e1a59da14da544d3049f079
VolcanoCookies <franek.kg@gmail.com>
2020-05-12 09:21:03 +0200

    Changes

--- menuScript.js
+++ menuScript.js
@@ -1,8 +1,10 @@
+// Wait for window to be ready and all elements loaded
 $(window).on('ready', function() {
+	// Replace the element with class nav with the one modular one
 	$('#nav').load(
 		'https://raw.githubusercontent.com/VolcanoCookies/Portfolio-Website/master/templates/navTemplate.html'
 	);
-//	$('#footer').load(
-//		'https://raw.githubusercontent.com/VolcanoCookies/Medusa-Network/master/templates/footerTemplate.html'
-//	);
+	//	$('#footer').load(
+	//		'https://raw.githubusercontent.com/VolcanoCookies/Medusa-Network/master/templates/footerTemplate.html'
+	//	);
 });
